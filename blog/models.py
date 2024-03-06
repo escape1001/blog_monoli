fa0b7be1ed46212cbb511d4ac4f95e963ab8cbe9
ES, Choi <69956156+escape1001@users.noreply.github.com>
2024-03-06 23:06:06 +0900

    "update" 블로그 모델 작성 후 마이그레이션

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    title=models.CharField(max_length=50)
+    contents=models.TextField()
+    thumbnail_image = models.ImageField(
+        upload_to="tube/images/%Y/%m/%d/", blank=True
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE
+    )
+    tags = models.ManyToManyField('Tag', blank=True)
+
+    def __str__(self):
+        return self.title
+    
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name='comments'
+    )
+    contents = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.contents
+
+
+class Like(models.Model):
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name='likes'
+    )
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return f"{self.post} / {self.author}"
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    
+    def __str__(self):
+        return self.name
