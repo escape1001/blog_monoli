2f703be2a5ac2d57330959ea5a74dbbcfb4b2dfb
ES, Choi <69956156+escape1001@users.noreply.github.com>
2024-03-07 16:55:30 +0900

    "update" blog_detail 기능연결

--- blog/urls.py
+++ blog/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path('create/', views.post_create, name="post_create"),
     path('update/<int:pk>/', views.post_update, name="post_update"),
     path('delete/<int:pk>/', views.post_delete, name="post_delete"),
+    path('update_comment/<int:pk>/', views.comment_update, name="comment_update"),
+    path('delete_comment/<int:pk>/', views.comment_delete, name="comment_delete"),
+    path('like/<int:pk>/', views.post_like, name="post_like"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,16 @@
 from django.views.generic import (
-    ListView, DetailView, CreateView, UpdateView, DeleteView
+    ListView, DetailView, CreateView, UpdateView, DeleteView, View
 )
-from .models import Post, Comment, Tag
+from .models import Post, Comment, Tag, Like
 from django.urls import reverse_lazy, reverse
 from django.db.models import Q
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.db.models import Count
 from .forms import CommentForm
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
 
 class Main(ListView):
@@ -18,7 +22,6 @@ class Main(ListView):
         
         # 최신 글 상위 6개
         latest_list = Post.objects.all().order_by('-created_at')[:6]
-
         # 인기글 상위 6개 (여기서는 댓글 개수로 인기를 판단하도록 예시로 설정)
         popular_list = Post.objects.annotate(num_comments=Count('likes')).order_by('-num_comments')[:6]
 
@@ -53,21 +56,30 @@ class PostDetail(DetailView):
     model = Post
 
     def get_context_data(self, **kwargs):
+        author = self.request.user
         context = super().get_context_data(**kwargs)
         context['form'] = CommentForm()
         
-        current_post_pk = self.object.pk
-        prev_post = Post.objects.filter(pk__lt=current_post_pk).order_by('-pk').first()
-        next_post = Post.objects.filter(pk__gt=current_post_pk).order_by('-pk').first()
+        current_post = self.object
+        prev_post = Post.objects.filter(pk__lt=current_post.pk).order_by('-pk').first()
+        next_post = Post.objects.filter(pk__gt=current_post.pk).order_by('-pk').first()
+
+        try :
+            liked = bool(Like.objects.filter(post=current_post, author=author))
+            context['liked'] = liked
+        except:
+            pass
 
         context['prev_post'] = prev_post
         context['next_post'] = next_post
         
         return context
     
+    @method_decorator(login_required)
     def post(self, request, *args, **kwargs):
         self.object = self.get_object()
         form = CommentForm(request.POST)
+
         if form.is_valid():
             author = request.user
             contents = form.cleaned_data["contents"]
@@ -106,9 +118,51 @@ class PostDelete(UserPassesTestMixin, DeleteView):
         return self.request.user == self.get_object().author
 
 
+class CommentUpdate(UserPassesTestMixin, UpdateView):
+    model = Comment
+    fields = ['contents']
+
+    def test_func(self):
+        return self.request.user == self.get_object().author
+
+    def get_success_url(self):
+        return reverse('post_detail', args=[str(self.object.post.pk)])
+    
+class CommentDelete(UserPassesTestMixin, DeleteView):
+    model = Comment
+
+    def get_success_url(self):
+        return reverse('post_detail', args=[str(self.object.post.pk)])
+
+    def test_func(self):
+        return self.request.user == self.get_object().author
+    
+
+class LikeToggle(LoginRequiredMixin, View):
+    model = Like
+    
+    def post(self, request, *args, **kwargs):  
+        post_pk = kwargs.get('pk')
+        author = request.user
+
+        try:
+            like_obj = Like.objects.get(post_id=post_pk, author=author)
+            like_obj.delete()
+
+        except Like.DoesNotExist:
+            like_obj = Like.objects.create(post_id=post_pk, author=author)
+            like_obj.save()
+
+        return HttpResponseRedirect(reverse('post_detail', args=[str(post_pk)]))
+
+
+
 main = Main.as_view()
 post_list = PostList.as_view()
 post_detail = PostDetail.as_view()
 post_create = PostCreate.as_view()
 post_update = PostUpdate.as_view()
 post_delete = PostDelete.as_view()
+comment_update = CommentUpdate.as_view()
+comment_delete = CommentDelete.as_view()
+post_like = LikeToggle.as_view()
